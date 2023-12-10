971459b810ef483945bb9d5bc72e405abf9aa2d3
saamiirr <mohamedsamir3333333@gmail.com>
2023-12-10 13:56:35 +0200

    new API key

--- src/js/config.js
+++ src/js/config.js
@@ -1,5 +1,5 @@
 export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
 export const TIMEOUT_SEC = 10;
 export const RES_PRE_PAGE = 10;
-export const KEY = '4bf7aca1-a953-4da8-9a42-c839964333f3';
+export const KEY = '9e12bbd6-e07c-4e85-b48f-d89b74eaead4';
 export const MODAL_CLOSE_SEC = 2.5;
